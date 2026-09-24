8fcfbdb8b1fbbad6ac7f5609b30f8a98bbf5d1ba
jandrespb <jandrespb.07@gmail.com>
2026-09-24 17:29:06 -0500

    fix: :hammer: config link header dashboard and create basic template user

--- src/app/dashboard/components/dashboard-front-component/header/header.ts
+++ src/app/dashboard/components/dashboard-front-component/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'dashboard-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
 })
 export class DashboardHeaderComponent {}
